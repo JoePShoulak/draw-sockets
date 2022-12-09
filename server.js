42d2bc11b035847412eeae94317361577c3c4df7
Joe P Shoulak <joe@squarecompass.io>
2022-12-09 00:31:16 -0600

    fix heroku?

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const socket = require("socket.io");
 
-const PORT = 3001;
+const PORT = process.env.PORT ?? 3001;
 
 const app = express();
 app.use(express.static("public"));
